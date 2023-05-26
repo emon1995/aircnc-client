8e5ff7a384739895ccaa54d724800d99f606d9b4
emon1995 <emonhossain995@gmail.com>
2023-05-26 20:14:51 +0600

    categories added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/Shared/Navbar/Navbar";
 
 const Main = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <div className="pb-20 pt-28">
+        <Outlet />
+      </div>
     </div>
   );
 };
